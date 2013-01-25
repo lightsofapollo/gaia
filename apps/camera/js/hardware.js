43897397f8d59749d106b3db483803f6a3804437
James Lal <james@lightsofapollo.com>
2013-01-24 23:52:53 -0800

    more debug

--- apps/camera/js/hardware.js
+++ apps/camera/js/hardware.js
@@ -28,16 +28,21 @@
      * @param {Function} callback [camera].
      */
     get: function(idx, callback) {
+      var _start = Date.now();
+
       if (callback)
         this._pending.push(callback);
 
       this._cameras = navigator.mozCameras.getListOfCameras();
+      dump('[camera] list - ' + (Date.now() - _start) + ' ms\n');
+
       var options = {camera: this._cameras[idx]};
 
       if (idx === this._idx) {
         setTimeout(handlePending, this._current);
       } else {
         navigator.mozCameras.getCamera(options, function handleCamera(camera) {
+          dump('[camera] startup - ' + (Date.now() - _start) + ' ms\n');
           Hardware._current = camera;
           Hardware._idx = idx;
           handlePending(camera);
