aa6e80173a8345066241ffdec10227eae3d04315
James Lal <james@lightsofapollo.com>
2013-08-20 23:13:58 -0700

    Add fakeserver helpers

--- apps/email/test/marionette/send_imap_email_test.js
+++ apps/email/test/marionette/send_imap_email_test.js
@@ -1,14 +1,16 @@
 var Email = require('./email');
 var assert = require('assert');
+var serverHelper = require('./lib/server_helper');
 
 marionette('send email via IMAP', function() {
   var client = marionette.client();
   var app;
-
   setup(function() {
     app = new Email(client);
     app.launch();
   });
+
+  var server = serverHelper.use({}, this);
 
   test('should send a email', function() {
     const ONE_NEW_EMAIL_NOTIFICATION = '1 New Email';
